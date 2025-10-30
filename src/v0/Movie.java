3a0fa7d482cae52d2c39a6e03f3e37d706a19e02
José Cruz <josejmcruz10@gmail.com>
2025-10-30 12:20:01 +0000

    V7 para a V8

--- src/v0/Movie.java
+++ src/v0/Movie.java
@@ -2,53 +2,20 @@ package v0;
 
 public class Movie {
 
-	public enum Code {REGULAR, CHILDRENS, NEW_RELEASE};
-	
-	private String _title;
-	private Code _priceCode;
-	
-	public Movie(String title, Code priceCode) {
-		_title = title;
-		_priceCode = priceCode;
-	}
+    private String _title;
+    private Price	_price;
 
-	public String getTitle() {
-		return _title;
-	}
+    public Movie(String title, Price price) {
+        _title = title;
+        _price = price;
+    }
 
-	public Code getPriceCode() {
-		return _priceCode;
-	}
+    public String getTitle() {
+        return _title;
+    }
 
-	public int getFrequentRentalPoints(int duration)
-	{
-		return (_priceCode == v0.Movie.Code.NEW_RELEASE) && duration > 1 ? 2 : 1;
-	}
-
-	public double getRentalAmount(int duration)
-	{
-		double result = 0;
-
-		switch (_priceCode)
-		{
-			case REGULAR:
-				result += 2;
-				if (duration > 2)
-					result += (duration - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				result += duration * 3;
-				break;
-			case CHILDRENS:
-				result += 1.5;
-				if (duration > 3)
-					result += (duration - 3) * 1.5;
-				break;
-		}
-		return result;
-	}
-
-
-
-
+    public Price getPrice()
+    {
+        return _price;
+    }
 }
